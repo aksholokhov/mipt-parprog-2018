ebbb2d9db842f833fc8d7459e15bbcfdd78ffc1c
Alexey Sholokhov <ak.sholokhov@gmail.com>
2017-03-01 17:47:29 +0300

    Task 1 finished

--- task1/task1.c
+++ task1/task1.c
@@ -8,30 +8,16 @@ double f(double x) {
     return 1/(1+x*x);
 }
 
-double calculate_integral2(double a, double b, double step) {
-    if (a == b) return 0;
-    double x1 = a, x2 = x1+step;
-    double sum = 0;
-    while(x2 <= b) {
-        double result = (f(x1)+f(x2))/2*step;
-      //  printf("%f\n", result);
-        sum += result;
-        x1 = x2;
-        x2 += step;
-    }
-    //printf("sum: %.10f\n", sum);
-    return sum;
-}
-
 double calculate_integral(double a, double b, double step)
 {
-    int i=0;
-    double int_value=0;
+    int i = 0;
+    double result=0;
     for(i=0; i<(b-a)/step; i++)
     {
-        int_value += ( f( a + step * i ) + f( a + ( i + 1 ) * step ) ) * step /2.0;
+        result += (f(a+step*i) + f(a+(i+1)*step))*step/2.0;
     }
-    return int_value;
+    result += (f(b)+f(a+step*i))*(b-a-step*i) /2.0;
+    return result;
 }
 
 
@@ -48,14 +34,15 @@ int main(int argc, char const *argv[]) {
 
     if (my_rank == 0) {
         double begin = MPI_Wtime();
-        double result_single_thread = calculate_integral(a, b, (b-a)/N);
+        double result_single_thread = calculate_integral(a, b, step);
         double end = MPI_Wtime();
-        printf("The single-process result is %.10f, with the time: %f secs\n", result_single_thread, end-begin);
+ 
+        double t1 = end - begin;
+        printf("The single-process result is %.10f, with the time: %f secs\n", result_single_thread, t1);
 
         begin = MPI_Wtime();
         
         int npp = N/size;
-
 
         double x1 = a, x2 = a+step*npp;
         for (int i = 1; i < size; i++) {
@@ -69,16 +56,19 @@ int main(int argc, char const *argv[]) {
 
         double my_result = calculate_integral(x1, b, step);
         double result = my_result;
-
         double buf;
         MPI_Status status;
         for (int i = 1; i < size; i++) {
             MPI_Recv(&buf, 1, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
+          //  printf("%d: %.10f \n", i, buf);
             result += buf;
         }
 
+        //printf("%d: %.10f \n", my_rank, my_result);
+
         end = MPI_Wtime();
-        printf("%d: The multiple-process result is %.10f with the time: %f\n", my_rank, result, end-begin); 
+        //printf("%d: The multiple-process result is %.10f with the time: %f\n", my_rank, result, end-begin); 
+        printf("%f\n", t1/(end - begin));
 
     }
      if (my_rank != 0) {
