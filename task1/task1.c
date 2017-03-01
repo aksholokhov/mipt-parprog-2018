11c24164574e39d05115c6846259cee40c5b3d59
Alexey Sholokhov <ak.sholokhov@gmail.com>
2017-03-01 16:09:11 +0300

    Simplify task1 code

--- task1/task1.c
+++ task1/task1.c
@@ -8,7 +8,7 @@ double f(double x) {
     return 1/(1+x*x);
 }
 
-double calculate_integral(double a, double b, double step) {
+double calculate_integral2(double a, double b, double step) {
     if (a == b) return 0;
     double x1 = a, x2 = x1+step;
     double sum = 0;
@@ -23,11 +23,23 @@ double calculate_integral(double a, double b, double step) {
     return sum;
 }
 
+double calculate_integral(double a, double b, double step)
+{
+    int i=0;
+    double int_value=0;
+    for(i=0; i<(b-a)/step; i++)
+    {
+        int_value += ( f( a + step * i ) + f( a + ( i + 1 ) * step ) ) * step /2.0;
+    }
+    return int_value;
+}
+
+
+
 int main(int argc, char const *argv[]) {
     int N = atoi(argv[1]);
     double a = 0, b = 1;
-    calculate_integral(a, b, (b-a)/N);
-    //return 0;
+    double step = (b-a)/N;
     MPI_Init(&argc, &argv);
     int size = 0;   
     int my_rank = 0;
@@ -41,48 +53,41 @@ int main(int argc, char const *argv[]) {
         printf("The single-process result is %.10f, with the time: %f secs\n", result_single_thread, end-begin);
 
         begin = MPI_Wtime();
-        //TODO: FIX THIS 
-        int N_per_proc = (int)(((float)N)/size);
-        //int N_per_proc = N/size;
-        N_per_proc = N_per_proc == 0 ? 1 : N_per_proc;
-        printf("NPP: %d\n", N_per_proc);
-        double step = (b-a)/N;
-        double x1 = a+N_per_proc*step, x2 = x1+N_per_proc*step;
         
-        double* buf = (double*)malloc(3*sizeof(double));
+        int npp = N/size;
+
+
+        double x1 = a, x2 = a+step*npp;
         for (int i = 1; i < size; i++) {
-            buf[1] = i == size -1 ? b : x2;
-            //buf[1] = x2;
-            buf[0] = i < N? x1 : buf[1];
-            buf[2] = step;
-            printf("%d to %d: %.10f - %.10f, %.5f\n", my_rank, i, buf[0], buf[1], (buf[1]-buf[0])/step);
-            MPI_Send(&buf[0], 3, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
+            double* buf = (double*)malloc(2*sizeof(double));
+            buf[0] = x1;
+            buf[1] = x2;
+            MPI_Send(&buf[0], 2, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
             x1 = x2;
-            x2 += step*N_per_proc;
+            x2 += step*npp;
         }
-        printf("%d to %d: %.10f - %.10f\n", my_rank, 0, a, a+N_per_proc*step);
-        double my_result = calculate_integral(a, a+N_per_proc*step, step);
+
+        double my_result = calculate_integral(x1, b, step);
         double result = my_result;
-        MPI_Status Status;
-        printf("%d: %.10f\n", my_rank, my_result); 
+
+        double buf;
+        MPI_Status status;
         for (int i = 1; i < size; i++) {
-            MPI_Recv(&buf[0], 1, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &Status);
-            printf("%d: %.10f\n", i, buf[0]);
-            result += buf[0];
+            MPI_Recv(&buf, 1, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
+            result += buf;
         }
-        
+
         end = MPI_Wtime();
         printf("%d: The multiple-process result is %.10f with the time: %f\n", my_rank, result, end-begin); 
 
     }
-    if (my_rank != 0) {
-        double* buf = (double*)malloc(3*sizeof(double));
+     if (my_rank != 0) {
+        double* buf = (double*)malloc(2*sizeof(double));
         MPI_Status Status;
-        MPI_Recv(&buf[0], 3, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD, &Status);
-        double result = calculate_integral(buf[0], buf[1], buf[2]);
-        buf[0] = result;
-        MPI_Send(&buf[0], 1, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD);
-    }
+        MPI_Recv(&buf[0], 2, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD, &Status);
+        double result = calculate_integral(buf[0], buf[1], step);
+        MPI_Send(&result, 1, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD);
+    } 
 
     MPI_Finalize();
     return 0;
