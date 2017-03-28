f00f6e05acfb07597f2be62204a83a19ee6bda89
Alex Sholokhov <ak.sholokhov@gmail.com>
2017-03-28 16:32:47 +0300

    Task 2 first working version

--- task2/task2.c
+++ task2/task2.c
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    float T = 0.005, h = 0.02, k = 1, dt = 0.0002, step_log = 0.1, u0 = 0, u1 = 1;
+    float T = 0.1, h = 0.02, k = 1, dt = 0.0002, step_log = 0.1, u0 = 0, u1 = 1;
 
     int points_num = (int)(1/h);
     int points_per_proc = points_num/(size-1);
@@ -47,26 +47,22 @@ int main(int argc, char** argv) {
             MPI_Recv(&points[points_per_proc+1], 1, MPI_DOUBLE, (rank+1) % size, 0,
                     MPI_COMM_WORLD, &status);
             buf[points_per_proc] = recalculate(&points[points_per_proc - 1], k, dt, h);
-
+            MPI_Send(&points[points_per_proc], 1, MPI_DOUBLE, (rank+1) % size, 0,
+                    MPI_COMM_WORLD);
             for (int j = 1; j < points_per_proc+1; j++) {
                 points[j] = buf[j];
             }
-
-            MPI_Send(&points[points_per_proc], 1, MPI_DOUBLE, (rank+1) % size, 0,
-                    MPI_COMM_WORLD);
         }
         free(points);
         free(buf);
     }
 
     if (rank == 0) {
-        double buf[2], u[2];
-        u[0] = u0;
-        u[1] = u0;
+        double buf[2];
         for (int i = 0; i < steps; i++) {
             MPI_Recv(buf, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &status);
-            MPI_Send(u, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
-            MPI_Send(u, 1, MPI_DOUBLE, size-1, 0, MPI_COMM_WORLD);
+            MPI_Send(&u0, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&u0, 1, MPI_DOUBLE, size-1, 0, MPI_COMM_WORLD);
             printf("%.3f ", u0);
             for (int j = 1; j < size; j++) {
                 MPI_Recv(buf, 2, MPI_DOUBLE, j, 1, MPI_COMM_WORLD, &status);
